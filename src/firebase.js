e2f45168d53554911ec4d88f9b092cb12e0af763
Joraku2173 <125777613+Joraku2173@users.noreply.github.com>
2023-08-10 20:38:04 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const firebaseConfig = {
+    apiKey: "AIzaSyAUpMPcWxXg5zr91XoiCRF89vxcioTCS5s",
+    authDomain: "react-ui-ux-practice-site.firebaseapp.com",
+    projectId: "react-ui-ux-practice-site",
+    storageBucket: "react-ui-ux-practice-site.appspot.com",
+    messagingSenderId: "162658851086",
+    appId: "1:162658851086:web:2ec1d33f833975fc091f5f"
+  };  
